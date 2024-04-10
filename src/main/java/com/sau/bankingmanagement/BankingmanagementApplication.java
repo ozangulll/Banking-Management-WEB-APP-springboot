ac646c3caa1651a4e89c2fbe3523bd003a8c9acf
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-10 14:46:38 +0300

    initial project created.

---
+++ src/main/java/com/sau/bankingmanagement/BankingmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.sau.bankingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sau/bankingmanagement/BankingmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sau.bankingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
