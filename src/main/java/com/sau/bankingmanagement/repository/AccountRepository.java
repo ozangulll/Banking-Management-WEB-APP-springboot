ebb83a501973b023671d4030914fb46fcc0679e2
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-11 13:16:53 +0300

    some findby*** methods added.

--- src/main/java/com/sau/bankingmanagement/repository/AccountRepository.java
+++ src/main/java/com/sau/bankingmanagement/repository/AccountRepository.java
@@ -3,5 +3,9 @@ package com.sau.bankingmanagement.repository;
 import com.sau.bankingmanagement.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account,Integer> {
+
+    Optional<Account> findByBranch(Integer integer);
 }
--- src/main/java/com/sau/bankingmanagement/repository/CustomerRepository.java
+++ src/main/java/com/sau/bankingmanagement/repository/CustomerRepository.java
@@ -3,6 +3,13 @@ package com.sau.bankingmanagement.repository;
 import com.sau.bankingmanagement.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository <Customer,Integer>{
+
+Optional<Customer> findbyAdress(String address);
+//optional means it can be nullable or not.
+// A container object which may or may not contain a non-null value.
+// eğer null ise isPresent() false döndürecek. bu işlevi controller tarafında kullanalım. Unutma
 
 }
