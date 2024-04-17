b927c11bd4ef13c16f5bbba0dabce1649c47c48c
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-04-17 17:33:20 +0300

    CRUD operations has been added.

--- src/main/java/com/sau/bankingmanagement/controllers/AccountController.java
+++ src/main/java/com/sau/bankingmanagement/controllers/AccountController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AccountController {
@@ -21,5 +25,39 @@ private AccountRepository accountRepository;
         List<Account> accounts=accountRepository.findAll();
         model.addAttribute("accounts",accounts);
         return "accounts-list";
+    }
+    @GetMapping("/accounts/add")
+    public String AddAccountForm(Model model){
+        Account account=new Account();
+        model.addAttribute("account",account);
+        return "add-account";
+    }
+    @PostMapping("/accounts/add")
+    public String AddAccount(@ModelAttribute("account") Account account){
+    accountRepository.save(account);
+    return "redirect:/accounts";
+    }
+    @GetMapping("/accounts/delete/{id}")
+    public String DeleteAccountScreen(@PathVariable("id") int id, Model model){
+        Optional<Account> account = accountRepository.findById(id);
+        model.addAttribute("account", account);
+        return "delete-screen";
+    }
+    @PostMapping("/accounts/delete/{id}")
+    public String deleteAccount(@PathVariable("id") int id){
+        accountRepository.deleteById(id);
+        return "redirect:/accounts";
+    }
+    @GetMapping("accounts/update/{id}")
+    public  String UpdateAccountForm(@PathVariable("id") int id,Model model){
+        Optional<Account> account=accountRepository.findById(id);
+        model.addAttribute("account",account);
+        return "update-account";
+    }
+    @PostMapping("accounts/update/{id}")
+    public  String UpdateAccount(@ModelAttribute("account") Account account){
+accountRepository.save(account);
+return "redirect:/accounts";
+
     }
 }
